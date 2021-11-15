3a722b5d6568cb617179eef20a8f2b4b6840f482
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-15 05:22:44 -0300

    feat: implements form whit inputs

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,11 @@
 import { Container, ContainerInput } from "./styles";
 
-function Input({ icon: Icon }) {
+function Input({ icon: Icon, register, name, ...rest }) {
   return (
     <Container>
       <ContainerInput>
         {Icon && <Icon />}
-        <input />
+        <input {...register(name)} {...rest} />
       </ContainerInput>
     </Container>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,58 @@
-import Form from "../../components/Form";
+import { Container } from "./styles";
+import {
+  FiUser,
+  FiMail,
+  FiLock,
+  FiPhone,
+  FiBook,
+  FiArrowRight,
+} from "react-icons/fi";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 function Home() {
-  return <Form />;
+  const formSchema = yup.object().shape({
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    password: yup.string().required("Senha obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container onSubmit={handleSubmit(onSubmitFunction)}>
+      <Input
+        icon={FiUser}
+        type="email"
+        placeholder="Email"
+        register={register}
+        name="email"
+        error={errors.email?.message}
+      />
+      <Input
+        icon={FiLock}
+        type="password"
+        placeholder="Senha"
+        register={register}
+        name="password"
+        error={errors.password?.message}
+      />
+      <Button type="submit">Login</Button>
+    </Container>
+  );
 }
 
 export default Home;
