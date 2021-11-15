c4357a1fca517e3608f150fd152d77b0d036eccb
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-15 11:43:29 -0300

    feat: implements page DASHBOARD

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,34 @@
+import { Container, ContainerData } from "./styles";
+import Button from "../../components/Button";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      {/* HEADER */}
+      <ContainerData>
+        <div>
+          <p>Kenzie Hub</p>
+          <img />
+        </div>
+      </ContainerData>
+
+      {/* MINHAS TECNOLOGIAS */}
+      <ContainerData>
+        <div>
+          <p>Minhas Tecnologias</p>
+          <Button>+</Button>
+        </div>
+      </ContainerData>
+
+      {/* MEUS TRABALHOS */}
+      <ContainerData>
+        <div>
+          <p>Meus trabalhos</p>
+          <Button>+</Button>
+        </div>
+      </ContainerData>
+    </Container>
+  );
 }
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContainerData = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
