bb6001c277b1f80aa2d49030870d9b8c8de83fe5
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-19 06:02:53 -0300

    fix: change hover buttons

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -19,7 +19,16 @@ export const Container = styled.button`
   border-radius: 10px;
   margin: 10px 0;
   cursor: pointer;
+  transition: background-color 1s;
 
   :hover {
+    background-color: ${(props) =>
+      props.color === "purple"
+        ? "#5a56e2"
+        : props.color === "green"
+        ? "#1dc177"
+        : "var(--grey-50)"};
+    color: ${(props) =>
+      props.color === "purple" ? "var(--white)" : "var(--white)"};
   }
 `;
